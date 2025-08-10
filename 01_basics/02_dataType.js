c94047afc8697acef87a137ae17cc9bd0daf654a
Subhasis Samantasinghar <117301784+Subhasis19@users.noreply.github.com>
2025-08-11 01:03:37 +0530

    Add initial implementation of data types in JavaScript

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer versoin
+
+// alert(3+3 ) // this will not work cuz , we are using node.js not browser
+
+let name ="Subhasis" //string
+let age = 23         //number 
+let isLogIn = true   //boolean [true / false]
+let something = null //null   
+let state  // undefined
+
+// null => it is a standalone value 
+//undefined =>
+// Symbol => unique 
+
+// Object
+
+console.log(typeof null); //object 
+console.log(typeof undefined);  // undefined
