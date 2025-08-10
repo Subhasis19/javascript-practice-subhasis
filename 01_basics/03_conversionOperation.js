fdd5599ccd1c399a783dbb28ecfde6baa090d0d2
Subhasis Samantasinghar <117301784+Subhasis19@users.noreply.github.com>
2025-08-11 01:26:13 +0530

    Add conversion operations and type checks in JavaScript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33asc" 
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber); //NaN  / Not a Number 
+console.log(typeof (valueInNumber));
+
+
+
+// "33" => 33
+// "33abc" => NaN /Not a Number 
+// true =>1 ; false => 0
+
+
+let isLogedIn = 1 
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+console.log(booleanIsLogedIn); //true 
+
+// 1 => true ; 0 => false
+// "" => false
+// "Subhasis" => true
+
+
+let num =22
+let stringNum = String(num)
+console.log(stringNum);
+console.log(typeof(stringNum));
+
+
+
