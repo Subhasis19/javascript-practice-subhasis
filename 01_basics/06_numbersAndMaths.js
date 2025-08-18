e57e224fded4e5416a2c2669414113aabbedb9e8
Subhasis Samantasinghar <117301784+Subhasis19@users.noreply.github.com>
2025-08-19 01:30:39 +0530

    Add initial implementation of number operations and random number generation in JavaScript

---
+++ 01_basics/06_numbersAndMaths.js
@@ -0,0 +1,34 @@
+const score = 300
+// console.log(score); //300
+
+const balance = new Number (100)
+// console.log(balance);  //[Number: 100]
+
+// console.log(balance.toString().length); //3
+// console.log(balance.toFixed(2));  // 100.00
+
+const otherNumber = 123.8345
+
+// console.log(otherNumber.toPrecision(3)); //it will round off the number and return => 124
+
+const num = 1000000
+// console.log(num.toLocaleString('en-IN')); // => 10,00,000 
+
+// ++++++++++++++++++++++++  Maths  ++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));  //4
+// console.log(Math.round(4.3)); //4
+// console.log(Math.ceil(4.3));//5
+// console.log(Math.floor(4.8)); //4
+
+// console.log(Math.pow(7, 2)); // 49
+// console.log(Math.sqrt(49));  // 7
+
+console.log(Math.random());
+console.log((Math.random() *10) +1 );
+console.log(Math.floor(Math.random() *10) +1 );
+
+
+
+
