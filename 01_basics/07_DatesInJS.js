8ec6e26aeaca1344310a5a6f3e871176307d4c91
Subhasis Samantasinghar <117301784+Subhasis19@users.noreply.github.com>
2025-08-22 15:32:29 +0530

    Add date manipulation examples and logging in JavaScript

---
+++ 01_basics/07_DatesInJS.js
@@ -0,0 +1,32 @@
+// Dates
+
+let Mydate = new Date()
+// console.log(Mydate.toString());
+// console.log(Mydate.toISOString());
+// console.log(Mydate.toLocaleString());
+// console.log(Mydate.toUTCString());
+// console.log(Mydate.getDate());
+// console.log(Mydate.toJSON());
+// console.log(typeof Mydate);
+
+// let MyCreatedDate = new Date(2023 , 1 , 28)
+// let MyCreatedDate = new Date(2023 , 1 , 28, 5,3) 
+// let MyCreatedDate = new Date("2023-01-02") 
+let MyCreatedDate = new Date("01-02-2023") 
+// console.log(MyCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// Logs the current timestamp in milliseconds
+// Get the current Unix timestamp in seconds by dividing milliseconds by 1000 and rounding down
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth() + 1x)
+console.log(newDate.getFullYear())
+console.log(newDate.getDate())
+console.log(newDate.getDay())
+
+
+
+
