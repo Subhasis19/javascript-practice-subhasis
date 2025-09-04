f4b358c766dac36e02b6f8fabf7fec13d738e6aa
Subhasis Samantasinghar <117301784+Subhasis19@users.noreply.github.com>
2025-09-05 01:01:08 +0530

    Refactor and enhance function examples with additional comments and logging

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -36,4 +36,37 @@ function loginUserMessage1(username = "Sam"){  // you can also give some default
 
 // loginUserMessage("SAM")
 // console.log (loginUserMessage1("Subhasis"))
-console.log (loginUserMessage1())
+// console.log (loginUserMessage1())
+
+
+
+function calculateCartPrice ( ...num ){
+    return num
+} 
+
+// console.log(calculateCartPrice(1232,4332512,423432,345));
+
+// The rest operator in JavaScript, denoted by three consecutive dots (...), is a feature introduced in ES6 (ECMAScript 2015) that allows you to collect an indefinite number of arguments into an array. Its primary use case is within function parameters to handle a variable number of arguments. 
+
+
+let user  = {
+    name : "Subahsis",
+    price : 199
+}
+
+function handleObject(Anyobj){
+    console.log(`Username is ${Anyobj.name} and the price is ${Anyobj.price}`);
+}
+
+// handleObject(user)
+
+function returnSecondValue (arr ){
+    return arr[1]
+}
+
+const MynewArray = [1,245,321432,4124325,3456]
+
+// console.log(returnSecondValue(MynewArray))
+// we can also directly pass the array..
+console.log(returnSecondValue([123,2345,1234512,3412345,234]))
+
